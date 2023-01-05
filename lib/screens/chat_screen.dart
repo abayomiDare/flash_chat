c198c1ac3d5c5a0a3cad04aa8b5c04015fbada80
abayomiDare <01.abayomidare>
2023-01-05 18:03:23 +0100

    set up firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   static String ID = "chat_screen";
@@ -9,15 +10,20 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _fireStore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
+  final msgTextController = TextEditingController();
 
+  late String messageText;
   late User user;
 
   void getCurrentUSer() {
     final loggedInUser = _auth.currentUser;
 
     if (loggedInUser != null) {
-      user = loggedInUser;
+      setState(() {
+        user = loggedInUser;
+      });
       print(user.email);
     }
   }
@@ -28,8 +34,29 @@ class _ChatScreenState extends State<ChatScreen> {
     super.initState();
   }
 
+  // void getMessages() async {
+  //   final messages = await _fireStore.collection('messages').get();
+  //   for (var message in messages.docs) {
+  //     print(message.data());
+  //   }
+  // }
+  // CollectionReference<Map<String, dynamic>> getDataStream() {
+  //   return _fireStore.collection('messages');
+  // }
+
+  // void getMessagesStream() {
+  //   CollectionReference msgCollection = _fireStore.collection('messages');
+  //   msgCollection.snapshots().listen((event) {
+  //     for (var message in event.docs) {
+  //       print(message.data());
+  //     }
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
+    print('kick of to print user email');
+    print(user.email);
     return Scaffold(
       appBar: AppBar(
         leading: null,
@@ -37,6 +64,7 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: const Icon(Icons.close),
               onPressed: () {
+                // getMessagesStream();
                 _auth.signOut();
                 Navigator.pop(context);
               }),
@@ -49,6 +77,10 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            MessageStream(
+              fireStore: _fireStore,
+              currentUser: user.email,
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -56,14 +88,20 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: msgTextController,
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   GestureDetector(
                     onTap: () {
+                      msgTextController.clear();
+                      _fireStore.collection('messages').add({
+                        'text': messageText,
+                        'sender': user.email,
+                      });
                       //Implement send functionality.
                     },
                     child: const Text(
@@ -81,4 +119,95 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 }
 
-mixin FirebaseUser {}
+class MessageStream extends StatelessWidget {
+  MessageStream(
+      {Key? key, required FirebaseFirestore fireStore, this.currentUser})
+      : _fireStore = fireStore,
+        super(key: key);
+
+  final FirebaseFirestore _fireStore;
+  var currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _fireStore.collection('messages').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        if (!snapshot.hasData) {
+          return const CircularProgressIndicator(
+            backgroundColor: Colors.lightBlueAccent,
+          );
+        }
+        final messages = snapshot.data?.docs.reversed;
+        List<MessageBubble> msgWidgets = [];
+        for (var msg in messages!) {
+          final msgText = msg.get('text');
+          final msgSender = msg.get('sender');
+
+          msgWidgets.add(
+            MessageBubble(
+                sender: msgSender,
+                text: msgText,
+                isMe: currentUser == msgSender ? true : false),
+          );
+        }
+        return Expanded(
+          child: ListView(
+              reverse: true,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              children: msgWidgets),
+        );
+      },
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble({required this.sender, required this.text, required this.isMe});
+  String sender;
+  String text;
+  bool isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            sender,
+            style: const TextStyle(fontSize: 12.0, color: Colors.black54),
+          ),
+          Material(
+            elevation: 5.0,
+            borderRadius: isMe
+                ? const BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0))
+                : const BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0)),
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Text(
+                text,
+                style: TextStyle(
+                    fontSize: 15.0, color: isMe ? Colors.white : Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
+// import 'package:modal_progress_hud/modal_progress_hud.dart';
 import '../constants.dart';
 import 'chat_screen.dart';
 
@@ -20,78 +20,77 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ModalProgressHUD(
-        inAsyncCall: showSpinner,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
-              const SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration:
-                    kTextFieldDecorator.copyWith(hintText: 'Enter your email'),
-              ),
-              const SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: kTextFieldDecorator.copyWith(
-                    hintText: 'Enter your password'),
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Material(
-                  color: Colors.lightBlueAccent,
-                  borderRadius: const BorderRadius.all(Radius.circular(30.0)),
-                  elevation: 5.0,
-                  child: MaterialButton(
-                    onPressed: () async {
-                      setState(() {
-                        showSpinner = true;
-                      });
-                      try {
-                        final newUser = await _auth.signInWithEmailAndPassword(
-                            email: email, password: password);
-                        print(newUser);
-                        if (newUser != null) {
-                          Navigator.pushNamed(context, ChatScreen.ID);
-                        }
-                        showSpinner = false;
-                      } catch (error) {
-                        print(error);
+      // ModalProgressHUD(
+      // inAsyncCall: showSpinner,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            SizedBox(
+              height: 200.0,
+              child: Image.asset('images/logo.png'),
+            ),
+            const SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (value) {
+                email = value;
+              },
+              decoration:
+                  kTextFieldDecorator.copyWith(hintText: 'Enter your email'),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              obscureText: true,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                password = value;
+              },
+              decoration:
+                  kTextFieldDecorator.copyWith(hintText: 'Enter your password'),
+            ),
+            const SizedBox(
+              height: 24.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.lightBlueAccent,
+                borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () async {
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    try {
+                      final newUser = await _auth.signInWithEmailAndPassword(
+                          email: email, password: password);
+                      print(newUser);
+                      if (newUser != null) {
+                        Navigator.pushNamed(context, ChatScreen.ID);
                       }
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: const Text(
-                      'Log In',
-                    ),
+                      showSpinner = false;
+                    } catch (error) {
+                      print(error);
+                    }
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: const Text(
+                    'Log In',
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
